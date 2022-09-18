5836bded023c6eb5db89d6cfce94f346364bd647
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-09-18 12:06:01 +0530

    remove unnecessary code

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,9 +7,7 @@ export const fetchCatImages = async (
   text: string,
   fetchCatImagesQueries: FetchCatImagesQueries
 ) => {
-  const path = `${text}?${new URLSearchParams(
-    fetchCatImagesQueries as any
-  ).toString()}`;
+  const path = `${text}?${new URLSearchParams(fetchCatImagesQueries as any)}`;
   try {
     const response = await axios.get(path, { responseType: "arraybuffer" });
     return { data: Buffer.from(response.data), status: response.status };
