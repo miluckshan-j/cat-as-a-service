9bf3157aeffed9afcfb30ce72741f3aaf3c375a2
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-09-18 11:24:40 +0530

    implement cat image fetch api

---
+++ src/types/api.types.ts
@@ -0,0 +1,6 @@
+export interface FetchCatImagesQueries {
+  width: number;
+  height: number;
+  color: string;
+  size: number;
+}
---
+++ src/utils/api.ts
@@ -0,0 +1,19 @@
+import { FetchCatImagesQueries } from "api.types";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://cataas.com/cat/says/";
+
+export const fetchCatImages = async (
+  text: string,
+  fetchCatImagesQueries: FetchCatImagesQueries
+) => {
+  const path = `${text}?${new URLSearchParams(
+    fetchCatImagesQueries as any
+  ).toString()}`;
+  try {
+    const response = await axios.get(path, { responseType: "arraybuffer" });
+    return { data: Buffer.from(response.data), status: response.status };
+  } catch (error) {
+    console.error("There was an issue fetching");
+  }
+};
